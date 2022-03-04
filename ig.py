0d2c637f3496b82a366b40728d59f793ca10d7b0
yassine20011 <medalgamer47@gmail.com>
2022-03-04 21:59:10 +0100

    updated

--- ig.py
+++ ig.py
@@ -30,7 +30,7 @@ password = components.password()
 
 def login_form():
 
-# Send selenium to find element by name and put generated Email in input  the specific for it.
+
 	time.sleep(1)
 	find_element_email = browser.find_element_by_name('emailOrPhone')
 	find_element_email.send_keys(items["email"])
@@ -38,24 +38,22 @@ def login_form():
 	with open("data.txt", "a") as log_in_info:
 		log_in_info.write("\nEmail : " + items["email"])
 
-# Send selenium to find element by name and put generated fullname in input  the specific for it.
+
 
 		time.sleep(2)
 		fullname = browser.find_element_by_name('fullName')
 		fullname.send_keys(username)
-# Send selenium to find element by name and put generated username in input  the specific for it.
+
 		time.sleep(2)
 		find_element_username = browser.find_element_by_name('username')
 		log_in_info.write("\nUsername : " + username)
 		find_element_username.send_keys(username)
-# Send selenium to find element by name and put generated password in input  the specific for it.
 		time.sleep(2)
 		find_element_password = browser.find_element_by_name('password')
 		log_in_info.write(f"Password : {password}")
 		log_in_info.write("\n#############################")
 		find_element_password.send_keys(password)
 
-# Find a the button with xpath and click it
 		time.sleep(5)
 
 		Register = browser.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div[1]/div/form/div[7]/div/button")
@@ -65,7 +63,6 @@ def login_form():
 
 def Birthday_form():
 
-	# Enter a valid Birthday
 	time.sleep(2)
 	option_value = str(random.randint(1975, 2001))
 	select_element = browser.find_element_by_xpath(
@@ -73,7 +70,7 @@ def Birthday_form():
 	select = Select(select_element)
 	select.select_by_value(option_value)
 	time.sleep(2)
-	# This to Click at button next
+
 	Click_button = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div[1]/div/div[6]/button')
 	Click_button.click()
 	
